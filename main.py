f1daea222c180bd3d8a11f1b2d2aec323f49b2b2
Vargur <40493346+VargurStorm@users.noreply.github.com>
2024-06-23 23:46:26 +0100

    temp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import csv
 import numpy as np
 import time
+import cProfile
+import pstats
 
 from core import *
 from core.units import *
@@ -16,11 +18,15 @@ def main():
     run_count = GameSettings.RUN_COUNT
 
     attackers = [
-        [(custodian_guard, 5)],
+        [(custodian_guard, 20)],
+        [(allarus_custodians, 20)],
+        [(custodian_guard_lethal, 20)],
+        [(custodian_guard_sustained, 20)],
+        [(custodian_guard_lethal_and_sustained, 20)],
         [(ork_boyz, 20)]
     ]
 
-    defenders = [meq, teq]
+    defenders = [meq, teq, custodian_guard, oeq, geq, ]
 
     scenario_list: list[Scenario] = build_scenarios(attackers, defenders)
 
@@ -30,10 +36,18 @@ def main():
         sim_list.append(this_sim)
 
     write_results_to_csv(sim_list)
-    create_sankey_from_scenario(scenario_list[0])
+    # create_sankey_from_scenario(scenario_list[0])
+
+
+def profile_main():
+    cProfile.run('main()', 'profile_output')
+
+    with open('profile_results.txt', 'w') as f:
+        p = pstats.Stats('profile_output', stream=f)
+        p.sort_stats('cumulative').print_stats()
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    main()
+    profile_main()
     print(f"Simulation took {time.time() - start_time} seconds")
